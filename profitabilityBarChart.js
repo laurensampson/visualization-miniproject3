30f3b5f8b3e20c7673cee095c5dc860e32da6046
Shirley817950 <65436682+Shirley817950@users.noreply.github.com>
2022-11-06 20:38:50 -0500

    Barchart with different lead studios

--- profitabilityBarChart.js
+++ profitabilityBarChart.js
@@ -18,8 +18,9 @@ function profitabilityBarChart(data, container) {
     const yScale = d3.scaleLinear()
         .range([height, 0]);
 
-    let xAxis = d3.axisBottom(xScale)
+    let xAxis = d3.axisBottom(xScale);
     let yAxis = d3.axisLeft(yScale)
+                  .ticks(4);
     
     var drawY = svg.append('g')
                    .attr('class', 'axis y-axis')
@@ -29,42 +30,32 @@ function profitabilityBarChart(data, container) {
    
    var xLabel = svg.append("text")
                     .attr("class", "x-axis-title")
-                    .attr('x', width-120)
+                    .attr('x', width-50)
                     .attr('y', height+40);
     var yLabel = svg.append("text")
                     .attr("class", "y-axis-title")
                     .attr('x', -25)
                     .attr('y', -10);
-    var colors = d3.scaleOrdinal()
-                    .domain(d3.extent(data, d=>d.LeadStudio))
-                    .range(d3.schemeTableau10);
 
     var leadStudio = new Set(data.map(function(array){return array.LeadStudio;}));;
-    var rect = svg.selectAll("rect.label")
-        .data(leadStudio)
-        .enter()
-        .append("rect");
-    
-    rect.attr("height", 10)
-        .attr("width", 10)
-        .attr("x", width - 200)
-        .attr("y", (d, i) => (i - 0.4) * 25)
-        .attr("fill", d => colors(d));
-    var labels = svg.selectAll(".text")
-        .data(leadStudio)
-        .enter()
-        .append("text")
-    labels.attr("x", width - 175)
-        .attr("y", (d, i) => i * 25)
-        .text(d => d);
+    console.log(leadStudio);
 
     function update(data) {
-        let studioTotal = d3.count(data, d=>d.LeadStudio);
-        xScale.domain(data.map(d => d.LeadStudio));
-        yScale.domain([0,studioTotal]);
-        let format = d3.format('.2f');
-        const bars = svg.selectAll('.bar').data(data, d => d.LeadStudio);
-
+        var studioCount = new Object();
+        leadStudio.forEach(element => {
+          studioCount[element] = data.filter(obj => obj.LeadStudio == element).length;
+        })
+        const values = Object.values(studioCount);
+        const max = Math.max(...values);
+        var items = Object.keys(studioCount).map((key) => {return [key, studioCount[key]]});
+        items.sort((first, second) => second[1] - first[1]);
+        var keys = items.map((e) => e[0]);
+        yScale.domain([0, max]);
+        xScale.domain(keys);
+        let bars = svg.selectAll(".bar")
+                      .remove()
+                      .exit()
+                      .data(data, d => d.LeadStudio);
         bars.enter()
             .append('rect')
             .attr('class', 'bar')
@@ -72,31 +63,13 @@ function profitabilityBarChart(data, container) {
             .attr("y", d => height)
             .attr("height", d => 0)
             .attr("width", xScale.bandwidth())
-            .on("mouseenter", (event, d) => {
-                const pos = d3.pointer(event, window);
-                d3.select(".tooltip3")
-                  .style("display", 'block')
-                  .style("left", pos[0] + 10 + "px")
-                  .style("top", pos[1] - 700 + "px")
-                  .html(`<p>Film: ${d.Film}<\p>
-                  <p>Genre: ${d.Genre}<\p>
-                  <p>Profitability: ${format(d.Profitability)}<\p>
-                  <p>Rotten Tomatoes: ${d.RottenTomatoes}<\p>
-                  <p>Lead Studio: ${d.LeadStudio}<\p>
-                  <p>Worldwide Gross: ${format(d.WorldwideGross)}<\p>`);
-                })
-                .on("mouseleave", (event, d) => {
-                    d3.select(".tooltip3")
-                    .style("display", 'none');
-                })
-                .merge(bars)
-                .transition()
-                .delay((d,i) => i * 150)
-                .duration(1000)
-                .attr("x", d => xScale(d.LeadStudio))
-                .attr('y', d => yScale(d.Profitability))
-                .attr('height', d => height - yScale(d.Profitability))
-                .attr('fill', d => colors(d.LeadStudio))
+            .merge(bars)
+            .transition()
+            .duration(2000)
+            .attr("x", d => xScale(d.LeadStudio))
+            .attr("y", d => yScale(studioCount[d.LeadStudio]))
+            .attr('height', d => height - yScale(studioCount[d.LeadStudio]))
+            .attr("fill", d3.schemeTableau10[0]);
         bars.exit()
             .remove();
         drawX.transition()
